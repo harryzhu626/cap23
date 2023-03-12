6f8ee791ed422363f648066e8fbe43f59b737868
Harry Zhu <harryzhu626@gmail.com>
2023-03-12 14:30:24 -0400

    updated py

--- tweetcollector.py
+++ tweetcollector.py
@@ -1,15 +1,28 @@
-# import twint 
+import snscrape.modules.twitter as sntwt
+import json 
 
-# c = twint.Config()
-# c.Username = 'from:@GenshinImpact'
-# c.Search = 'Dehya'
 
-# twint.run.Search(c)
-
-# given origin_handle and domain, extract tweet and insert to mongodb 
 def collect_tweet(origin, domain):
     
-    raise NotImplementedError
+    query_text = f'{origin} until:2021-01-10 lang:en'
 
-if __name__ == '__main__':
-    collect_tweet('genshinimpact', 'dehya')
+    query = sntwt.TwitterHashtagScraper(query_text)
+    count = 0
+
+    with open('tweet011021.json', 'w') as f:
+
+        for tweet in query.get_items():
+            #print(count, type(tweet))
+            print(f'{vars(tweet)}\n')
+            #count += 1
+            tweet = tweet.json()
+            json_obj = json.loads(tweet)
+            # print(tweet)
+            json.dump(json_obj, f)
+            f.write('\n')
+
+
+
+# snscrape guide
+# until: starts from the given date and goes backward in time
+#   e.g. until:2023-01-10 starts from 1-9 and goes to 1-8. 
