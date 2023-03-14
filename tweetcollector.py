77c812f07a84cf44ef0f54bc0df7f49e95cfe5c8
Harry Zhu <harryzhu626@gmail.com>
2023-03-14 09:28:12 -0400

    updated py

--- tweetcollector.py
+++ tweetcollector.py
@@ -1,28 +1,61 @@
 import snscrape.modules.twitter as sntwt
 import json 
 
+def new_function(origin, domain_list, dates, todb=False):
+    print('wtf')
+    for item in domain_list:
+        query_text = f'{origin} {item} since:{dates[0]} until:{dates[1]} lang:en'
 
-def collect_tweet(origin, domain):
-    
-    query_text = f'{origin} until:2021-01-10 lang:en'
+        print('query text: ', query_text)
+        query = sntwt.TwitterHashtagScraper(query_text)
+        count = 0
 
-    query = sntwt.TwitterHashtagScraper(query_text)
-    count = 0
+        if todb:
+            # if to database, output a generator
+            for tweet in query.get_items():
+                yield tweet.json()
+        else:
+            json_filename = f'tweet{origin}{item}{dates[0]}{dates[1]}.json'
+            print('json filename: ', json_filename)
 
-    with open('tweet011021.json', 'w') as f:
+            with open(json_filename, 'w') as f:
+                for tweet in query.get_items():
+                    print(count, type(tweet))
+                    print(f'{vars(tweet)}\n')
+                    count += 1
+                    tweet = tweet.json()
+                    json_obj = json.loads(tweet)
+                    json.dump(json_obj, f)
+                    f.write('\n')
 
-        for tweet in query.get_items():
-            #print(count, type(tweet))
-            print(f'{vars(tweet)}\n')
-            #count += 1
-            tweet = tweet.json()
-            json_obj = json.loads(tweet)
-            # print(tweet)
-            json.dump(json_obj, f)
-            f.write('\n')
+def collect_tweet(origin, domain_list, dates, todb=False):
+    text = input('type smth')
+    for item in domain_list:
+        query_text = f'{origin} {item} since:{dates[0]} until:{dates[1]} lang:en'
 
+        print('query text: ', query_text)
+        query = sntwt.TwitterHashtagScraper(query_text)
+        count = 0
+
+        if todb:
+            # if to database, output a generator 
+            for tweet in query.get_items():
+                yield tweet.json()
+        else:
+            json_filename = f'tweet{origin}{item}{dates[0]}{dates[1]}.json'
+            print('json filename: ', json_filename)
+
+            with open(json_filename, 'w') as f:
+                for tweet in query.get_items():
+                    print(count, type(tweet))
+                    print(f'{vars(tweet)}\n')
+                    count += 1
+                    tweet = tweet.json()
+                    json_obj = json.loads(tweet)
+                    json.dump(json_obj, f)
+                    f.write('\n')
 
 
 # snscrape guide
 # until: starts from the given date and goes backward in time
-#   e.g. until:2023-01-10 starts from 1-9 and goes to 1-8. 
+#   e.g. until:2023-01-10 starts from 1-9 and goes to 1-8, 1-7 and so on. 
--- tweettodb.py
+++ tweettodb.py
@@ -1,26 +1,28 @@
-from tweetcollector import collect_tweet
+from tweetcollector import collect_tweet, new_function
 from mongo import insert_tweets
 from dategenerator import create_dates
 
 origin_handle = 'genshinimpact'
 domain_list = [
-    'diluc', 
+    'diluc',
     'jean',
-    'dehya', 
+    #'dehya', 
 ]
+todb = False
 
 date_start_year = 2022
 date_start_month = 1
 date_end_year = 2022
-date_end_month = 12
+date_end_month = 2
 
 dates = create_dates(date_start_year, date_end_year, 
                      date_start_month, date_end_month)
+date_example = 'until:2023-01-10', 'since:2021-01-01'
 
-date_example = 'until:2023-01-10'
 
-print(dates)
-
-# if __name__ == '__main__':
-#     collect_tweet(origin_handle, domain_list, dates, todb=False)
+if __name__ == '__main__':
+    print('running')
+    tweets = new_function(origin_handle, domain_list, dates, todb=todb)
+    # tweets = collect_tweet(origin_handle, domain_list, dates, todb=todb)
+    print(type(tweets))
 #     insert_tweets()
